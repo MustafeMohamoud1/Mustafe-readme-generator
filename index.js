94b4c7d35d01d225a0733b7253d492b0b26e11e0
mustafemohamoud <mustyymo@outlook.com>
2023-11-29 22:15:36 +0000

    index.js complete with all functions

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of yout project?'
+    },
+    {
+        type: 'input',
+        name: 'motivation',
+        message: 'What was your motivation??'
+    },
+    {
+        type: 'input',
+        name: 'purpose',
+        message: 'Why did you build this project?'
+    },
+    {
+        type: 'input',
+        name: 'functionality',
+        message: 'What problem does it solve?'
+    },
+    {
+        type: 'input',
+        name: 'thoughts',
+        message: 'What makes your project stand out?'
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+      err ? console.error(err) : console.log('README.md generated successfully!')
+    );
+  }
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+      .prompt(questions)
+      .then((answers) => {
+        const markdown = generateMarkdown(answers);
+        writeToFile('README.md', markdown);
+      })
+      .catch((error) => console.error(error));
+  }
 
 // Function call to initialize app
 init();
